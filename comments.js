2691e02dba0cd9b05863c0e6817c13a35a044fc7
Mohammed Hamza <211220035@nitdelhi.ac.in>
2023-07-26 14:37:09 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Create a Web Server
+
+// 1. Import the express library
+const express = require('express');
+
+// 2. Create an instance of express
+const app = express();
+
+// 3. Define some routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/comments', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+// 4. Start the server
+app.listen(3000, () => console.log('Listening on port 3000...'));
